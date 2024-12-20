6aec113f5f4b868f150ee08dd587b70835721b65
Alberto Russo <a.russo@geckosoft.it>
2024-12-20 20:02:07 +0100

    feat: update for thinkpad

--- scripts/wofi-power.sh
+++ scripts/wofi-power.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-entries="Suspend\nReboot\nShutdown"
+entries="Logout\nSuspend\nReboot\nShutdown"
 
-selected=$(echo -e $entries|wofi --width 250 --height 170 --dmenu --cache-file /dev/null | awk '{print tolower($1)}')
+selected=$(echo -e $entries|wofi --width 250 --height 200 --dmenu --cache-file /dev/null | awk '{print tolower($1)}')
 
 case $selected in
   logout)
-    exec archlinux-logout;;
+    exec hyprctl dispatch exit;;
   suspend)
-    exec $(~/.config/scripts/lock.sh && sleep 2 && systemctl suspend);;
+    exec $(hyprlock && sleep 2 && systemctl suspend);;
   reboot)
     exec systemctl reboot;;
   shutdown)
