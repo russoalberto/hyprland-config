89da9e6b2ac1447d2a0bada4d4d397318e6bb799
Alberto Russo <a.russo@geckosoft.it>
2025-05-05 20:40:25 +0200

    feat: gruvbox

--- scripts/wofi-power.sh
+++ scripts/wofi-power.sh
@@ -2,13 +2,13 @@
 
 entries="Logout\nSuspend\nReboot\nShutdown"
 
-selected=$(echo -e $entries|wofi --width 250 --height 200 --dmenu --cache-file /dev/null | awk '{print tolower($1)}')
+selected=$(echo -e $entries|wofi --width 250 --height 230 --dmenu --cache-file /dev/null | awk '{print tolower($1)}')
 
 case $selected in
   logout)
     exec hyprctl dispatch exit;;
   suspend)
-    exec $(hyprlock && sleep 2 && systemctl suspend);;
+    exec systemctl suspend;;
   reboot)
     exec systemctl reboot;;
   shutdown)
